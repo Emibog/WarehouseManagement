24120d70453108b2d3ebd377c54e8095450910df
Bogdan <eminov.bogdan@mail.ru>
2023-12-05 22:41:33 +0300

    Добавлено автоматическое открытие MAMP

--- WarehouseManagement/Login.cs
+++ WarehouseManagement/Login.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,11 +17,50 @@ namespace WarehouseManagement
         public string userLogin;
         public string userPass;
         public string UserPost { get; private set; }
+        private string mampPath = @"..\..\MAMP\MAMP";
+
         public Login()
         {
             InitializeComponent();
+
+            // Запустить MAMP, если он не запущен
+            if (!IsMampRunning())
+            {
+                try
+                {
+                    //Установка параметров запуска
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        //Путь до программы
+                        FileName = mampPath,
+                        //Установка параметра для запуска в свернутом режиме
+                        WindowStyle = ProcessWindowStyle.Minimized
+                    };
+
+                    Process.Start(startInfo);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при запуске MAMP: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
+        /// <summary>
+        /// Проверка на наличие запущенного MAMP
+        /// </summary>
+        /// <returns></returns>
+        private bool IsMampRunning()
+        {
+            Process[] processes = Process.GetProcessesByName("MAMP");
+            return processes.Length > 0;
+        }
+
+        /// <summary>
+        /// Вход
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonLogin_Click(object sender, EventArgs e)
         {
             userLogin = textBoxLogin.Text;
@@ -28,6 +68,7 @@ namespace WarehouseManagement
 
             DB db = new DB();
 
+            // Проверка подключения к БД
             if (db.openConnection())
             {
                 DataTable table = new DataTable();
